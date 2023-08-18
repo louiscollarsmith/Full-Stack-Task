4e1d5a31636b4779f4f21f72dc0a03462c95427c
Abhijit <abhi.baldawa@gmail.com>
2023-08-18 15:29:48 +0100

    Defined rest api available definitions

---
+++ frontend/backend-services/product-management/rest-api.ts
@@ -0,0 +1,20 @@
+interface ProductDB {
+  id: string;
+  sku: string;
+  name: string;
+  price: number;
+  currency: string;
+  description: string;
+  photoURL: string;
+}
+
+namespace GetAllProducts {
+  export const method = "GET";
+  const path = "/products";
+
+  export const getUrl = () => path;
+
+  export type ApiResponseType = ProductDB;
+}
+
+export { GetAllProducts };
